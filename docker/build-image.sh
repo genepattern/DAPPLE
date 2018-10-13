f5f0499a4ac92594f9216a0bf44d813ea69125d0
peterjcarr <pcarr@broadinstitute.org>
2018-10-12 21:26:15 -0400

    added 'build-image' target to the build script, for v0.19 release

--- docker/build-image.sh
+++
@@ -1,5 +0,0 @@
-# Example command to create a new image from the docker file
-#docker build -f Dockerfile . --tag dapple
-
-## Example command to build and tag a new image from the docker file
-docker build -f Dockerfile . --tag dapple:0.19
--- docker/run-example.sh
+++ docker/run-example.sh
@@ -26,25 +26,17 @@ set -euo pipefail
 mkdir -p ${working_dir}
 rm -f ${working_dir}/*
 
-#
-# 'source_dir', default: ./source
-#
-#: ${source_dir=${dapple_dir}/docker/source}
 
 #
-# 'dapple_script', default: /web/wwwprod/htdocs/mpg/dapple/NewCode/BuildNetwork_slowTMP.py
+# 'dapple_script', default: /code/BuildNetwork_slowTMP.py
 #
 : ${dapple_script=/code/BuildNetwork_slowTMP.py}
 
 
 #
-# 'input_file', default: ../exampleInput
+# 'input_file', default: ../exampleInput2
 #
-#: ${input_file=${dapple_dir}/exampleInput}
 : ${input_file=${dapple_dir}/exampleInput2}
-#: ${input_file=${dapple_dir}/exampleInput_SNP_RA}
-#: ${input_file=${dapple_dir}/exampleInput_SNP_CD}
-
 
 echo "Running dapple command ..."
 echo "  working_dir: ${working_dir}"
@@ -63,26 +55,8 @@ python "${dapple_script}" \
   genome=18 \
   nearestgene=false 
 
-#  -v "${source_dir}/code":/code \
-
-  #/bin/bash -c "awk 'NR==FNR{s[\$1]=1;next}{if (s[\$1]==1) print }' Seeds <(gzip -dc /data/wingspan/hg18/*.ws.gz |) > SNPcheck"
-#head <(/bin/bash -c "gzip -dc /data/wingspan/hg18/*.ws.gz |")
-#awk 'NR==FNR{s[$1]=1;next}{if (s[$1]==1) print }' Seeds `/bin/bash -c "ls /data/wingspan/hg18/*.ws.gz | xargs gzip -dc"`
-#awk 'NR==FNR{s[$1]=1;next}{if (s[$1]==1) print }' Seeds <(/bin/bash -c "gzip -dc /data/wingspan/hg18/*.ws.gz")
-
-#ls /data/wingspan/hg18/chr1.ws.gz
-
-#  /bin/bash -c "ls /data/wingspan/hg18/*"
-
-#  -v "${source_dir}/data":/data \
-
-#  -v "${source_dir}/web/wwwprod/htdocs/mpg/dapple/NewCode":/web/wwwprod/htdocs/mpg/dapple/NewCode \
-#  -v "${source_dir}/fg/wgas2/rossin":/fg/wgas2/rossin \
-#  -v "${source_dir}/home/unix/rossin/DALY/PPI/NewCode":/home/unix/rossin/DALY/PPI/NewCode \
-
 set +x
 
-# >${working_dir}/stdout.txt 2>${working_dir}/stderr.txt
 exit_code=$?
 echo "Completed, see results in ${working_dir}"
 
